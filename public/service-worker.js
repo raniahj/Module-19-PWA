02a4db77df45269b34c36de96e046310a071e012
raniahj <raniahj06@gmail.com>
2022-03-25 03:12:15 -0400

    add cache

---
+++ public/service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function (e) {
+
+})
+
+const FILES_TO_CACHE = [
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js",
+    "./index.html",
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
